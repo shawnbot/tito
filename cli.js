879e3d6510cf77a75cfe1c78dd6f620e13e1da82
Shawn Allen <shawn.allen@gsa.gov>
2016-04-12 14:14:10 -0700

    clean up; add map/filter with fof()

--- cli.js
+++ cli.js
@@ -1,32 +1,29 @@
 #!/usr/bin/env node
-var multiline = require('multiline'),
-    fs = require('fs'),
-    tito = require('./'),
-    formats = tito.formats,
-    $0 = 'tito',
-    yargs = require('yargs')
-      .usage($0 + ' [options] [input] [output]')
-      .describe('read', 'the input format (see below)')
-        .default('read', 'ndjson')
-        .alias('read', 'r')
-      .describe('write', 'the output format (see below)')
-        .default('write', 'ndjson')
-        .alias('write', 'w')
-      .describe('in', 'the input filename')
-        .alias('in', 'i')
-      .describe('out', 'the output filename')
-        .alias('out', 'o')
-      .describe('filter', 'filter input by this data expression')
-        .alias('filter', 'f')
-      .describe('map', 'map input to this data expression')
-        .alias('map', 'm')
-      .describe('help', 'Show this help message.')
-      .describe('version', 'Print the version and exit')
-        .alias('version', 'v')
-      .alias('help', 'h')
-      .wrap(72),
-    options = yargs.argv,
-    args = options._;
+var $0 = 'tito';
+var yargs = require('yargs')
+  .usage($0 + ' [options] [input] [output]')
+  .describe('read', 'the input format (see below)')
+  .default('read', 'ndjson')
+  .alias('r', 'read')
+  .describe('write', 'the output format (see below)')
+  .default('write', 'ndjson')
+  .alias('w', 'write')
+  .describe('in', 'the input filename')
+  .alias('i', 'in')
+  .describe('out', 'the output filename')
+  .alias('o', 'out')
+  .describe('filter', 'filter input by this data expression')
+  .alias('f', 'filter')
+  .describe('map', 'map input to this data expression')
+  .alias('m', 'map')
+  .describe('help', 'Show this help message.')
+  .describe('version', 'Print the version and exit')
+  .alias('v', 'version')
+  .alias('h', 'help')
+  .wrap(72);
+
+var options = yargs.argv;
+var args = options._;
 
 if (options.version) {
   console.log($0, 'v' + tito.version);
@@ -39,15 +36,40 @@ if (options.help) {
   return process.exit(1);
 }
 
+var fs = require('fs');
+var tito = require('./');
+var formats = tito.formats;
+
 delete options._;
 delete options.$0;
 
-var input = options.in || args[0] || '/dev/stdin',
-    output = options.out || args[1] || '/dev/stdout',
-    read = formats.createReadStream(options.read),
-    write = formats.createWriteStream(options.write);
+var input = options.in || args[0] || '/dev/stdin';
+var output = options.out || args[1] || '/dev/stdout';
+var parse = formats.createReadStream(options.read);
+var format = formats.createWriteStream(options.write);
 
-fs.createReadStream(input)
-  .pipe(read)
-  .pipe(write)
+var stream = fs.createReadStream(input)
+  .pipe(parse);
+
+if (options.map || options.filter) {
+  var fof = require('fof');
+
+  var map = options.map;
+  if (fs.existsSync(map)) {
+    map = require(map);
+  }
+  var filter = options.filter;
+  if (fs.existsSync(filter)) {
+    map = require(filter);
+  }
+
+  var transform = fof.stream(map, {
+    filter: filter
+  });
+
+  stream = stream.pipe(transform);
+}
+
+stream = stream
+  .pipe(format)
   .pipe(fs.createWriteStream(output));
